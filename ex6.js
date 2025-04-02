5b2ef395a454af8a03b71199099f84893c92a086
Omar Salloum <omarsalloum08@gmail.com>
2025-04-02 20:21:50 +0200

    small fixes

--- ex6.js
+++ ex6.js
@@ -2,6 +2,6 @@ const input = document.getElementById("numberInput");
 const button = document.getElementById("incrementButton");
 
 button.addEventListener("click", function () {
-    let currentValue = parseInt(input.value, 10) || 0;
+    const currentValue = parseInt(input.value) || 0;
     input.value = currentValue + 1;
 });
